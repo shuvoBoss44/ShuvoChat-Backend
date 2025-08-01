b9519437097d1e54c7b99c8d54bda1739861e958
shuvoBoss44 <chakmashuvo2016@gmail.com>
2025-08-01 07:17:33 +0600

    changed chat controller

--- app.js
+++ app.js
@@ -8,21 +8,6 @@ const db = require('./src/utils/db');
 const CustomError = require('./src/error/CustomError');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const multer = require('multer');
-
-const storage = multer.memoryStorage();
-const upload = multer({
-    storage,
-    limits: { fileSize: 10 * 1024 * 1024 },
-    fileFilter: (req, file, cb) => {
-        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg'];
-        if (allowedTypes.includes(file.mimetype)) {
-            cb(null, true);
-        } else {
-            cb(new CustomError(400, 'Only JPEG, JPG, PNG, or GIF images are allowed'));
-        }
-    },
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
